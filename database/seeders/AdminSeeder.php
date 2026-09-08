3deedde7fdc514881e96a1999903cfe018f27c27
Muhammad AbdulRehman <167601390+mabdulrehmandev@users.noreply.github.com>
2026-09-08 11:37:45 +0500

    Remove car_id migration and simplify AdminSeeder by removing account insertion

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -14,19 +14,8 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        // Insert into accounts table
-        $account = Account::create([
-            'group_id' => 1,
-            'name' => 'Admin',
-            'phone' => '03001234567',
-            'account_type' => 'admin',
-            'address' => 'Lahore, Pakistan',
-            'created_by' => 1,
-        ]);
-
         // Insert into users table
         $user = User::create([
-            'group_id' => 1,
             'name' => 'Admin',
             'username' => 'admin',
             'email' => 'admin@rideflow.com',
